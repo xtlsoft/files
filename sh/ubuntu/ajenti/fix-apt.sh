f504d64029b6e650ddc647af1c936a89a13e8c6e
xtlsoft <wpproqoiqy@live.com>
2017-02-25 19:56:20 +0800

    Update fix-apt.sh

--- sh/ubuntu/ajenti/fix-apt.sh
+++ sh/ubuntu/ajenti/fix-apt.sh
@@ -4,8 +4,10 @@ echo "+-------Ajenti Panel Fix-------+"
 echo "| By xtl. Copyright 2016-2017  |"
 echo "+------------------------------+"
 echo "| [-]Getting Deb Package...    |"
-wget http://xtlsoft.github.io/files/sh/ubuntu/ajenti/apt-show-versions_1.0.0-0ubuntu1_all.deb  >> ./log.txt
+wget http://xtlsoft.github.io/files/sh/ubuntu/ajenti/apt-show-versions_1.0.0-0ubuntu1_all.deb
 echo "| [-]Unpacking Package...      |"
-dpkg ./fix-apt.deb >> ./log.txt
+dpkg -i ./apt-show-versions_1.0.0-0ubuntu1_all.deb
 echo "| [:]Fix Success.              |"
 echo "+------------------------------+"
+rm ./apt-show-versions_1.0.0-0ubuntu1_all.deb
+rm ./fix-apt.sh
